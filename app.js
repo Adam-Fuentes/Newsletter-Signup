9cf8fa70b96be8ac9a9c954e2847a6b8646cd0c8
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-11-02 11:34:42 +0100

    add post

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));//código necesario para el body parser
-
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    var firstName = req.body.fisrtName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+
+    console.log(firstName);
 });
 
 app.listen(3000, function(){
